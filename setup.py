4f232361c6fff44b54113e8fa00624cb1a0c82e1
kiki7000 <devkiki7000@gmail.com>
2021-04-10 20:19:28 +0900

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from os import environ
 from setuptools import setup, find_packages
 
-from discordex import __version__ as version
+from discordex import __version__ as version, __author__ as author, __license__ as license, __name__ as name
 
 setup(
-    name = 'dex.py',
-    version = version,
-    license = 'MIT',
-    author = 'kiki7000',
-    author_email = 'devkiki7000@gmail.com',
-    description = 'discord.py extension',
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/kiki7000/dex.py',
-    packages = find_packages(),
-    install_requires = open('requirements.txt').readlines(),
-    python_requires = '>=3.8'
+    name=name,
+    version=version,
+    license=license,
+    author=author,
+    author_email='devkiki7000@gmail.com',
+    description='discord.py extension',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Team-Kat/dex.py',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    python_requires='>=3.8'
 )
